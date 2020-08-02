1d08523d4c42708464e9d69eb1c1a3abbdc6a0ec
ELIO <2925539854@qq.com>
2020-08-02 23:44:02 +0800

    do an exercise

--- SICP_1.1.7-1.1.8.scm
+++ SICP_1.1.7-1.1.8.scm
@@ -19,11 +19,33 @@
 (define (newtoniter guess x)
   (if(good-enough? guess x)
      guess
-     (newtoniter (improve guess x)
-                 x)))
+     (newtoniter (improve guess x) x)))
 
 (define (sqrt x)
   (newtoniter 1.0 x))
 
 (display (sqrt 9))
-(exit)
+;;-----------------------------------------------------------
+;;Exercise 2
+(define (abs x)
+    (if (< x 0)
+        (- x)
+        x))
+
+(define (cube x)(* x x x))
+(define (square x)(* x x))
+(define (goodguess?  guess x)
+  (< (abs (- (cube guess) x)) 0.0000001))
+
+(define (improvecube guess x)
+  (/ (+(/ x (square guess)) (* 2 guess)) 3))
+
+(define (cuberoot guess x)
+  (if (goodguess? guess x)
+    guess
+    (cuberoot (improvecube guess x) x)))
+
+(define (trisqrt x)
+  (cuberoot 1.0 x))
+
+(display (trisqrt 8))
